a789b2f6ee3b323f52cec4bef4ccd7b524d51aa4
Thy96 <caodinhthy1996@gmail.com>
2018-07-12 10:04:33 +0700

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,20 +1,80 @@
-var Greeter = (function () {
+// var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+// // Put the object into storage
+// localStorage.setItem('testObject', JSON.stringify(testObject));
+
+// // Retrieve the object from storage
+// var retrievedObject = localStorage.getItem('testObject');
+
+// console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+
+var CurdList = (function () {
   'use strict';
-  function Greeter(message) {
+  function CurdList(message) {
     this.greeting = message;
     this.d = window.document;
+    var originStore = '{"1": {"name": "Hau"},"2": {"name": "Thy"},"3": {"name": "Toan"}]';
+
+    this.setStore("devlist", originStore);
   }
-  Greeter.prototype.greet = function () {
-    return "Hello, " + this.greeting;
-  };
-  Greeter.prototype.showMsg = function () {
-    this.d.querySelector('[data-message]').innerHTML = this.greeting;
-  };
-  return Greeter;
+
+/*
+{
+  "1": {"name": "Hau"},
+  "2": {"name": "Thy"},
+  "3": {"name": "Toan"}
+}
+*/
+
+  CurdList.prototype.getStore = function (storeName) {
+    var store = localStorage.getItem(storeName);
+    try
+    {
+      store = JSON.parse(store);
+    }
+    catch(e)
+    {
+      store = null;
+    }    
+    return store;
+  }
+
+  CurdList.prototype.setStore = function (storeName, data) {
+    localStorage.setItem(storeName, data);
+  }
+
+
+
+  CurdList.prototype.addItem = function () {
+    var that = this;
+    var addButton = document.querySelector('[data-btn-add]');
+    addButton.addEventListener("click", function () {
+      var newStore = {};
+      var oldStore = that.getStore('devlist');
+      
+      var devName = that.d.querySelector('[data-name]').value;
+      if ( devName.length === 0 ) return false; 
+      if ( oldStore === null ) {
+        newStore = {"1": {"name": devName}};
+      } else {
+        var objLength = Object.keys(oldStore).length;
+        newStore = oldStore;
+        newStore[objLength + 1] = {"name": devName};
+      }
+      localStorage.setItem("devlist", JSON.stringify(newStore));
+      document.querySelector('[data-name]').value = "";
+    });
+  }
+
+  return CurdList;
 }());
+
+
 
 // instance
 window.addEventListener('DOMContentLoaded', function () {
-  var wedding_greet = new Greeter('moi ban den an dam cuoi');
-  wedding_greet.showMsg();
+  
+  var add_name = new CurdList("hello word");
+  add_name.addItem();
 });
