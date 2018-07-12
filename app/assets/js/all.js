35e38f501cbb8dd33ce50210c9a47c9cf4d1d612
Thy96 <caodinhthy1996@gmail.com>
2018-07-12 15:54:16 +0700

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -15,8 +15,8 @@ var CurdList = (function () {
     this.greeting = message;
     this.d = window.document;
     var originStore = '{"1": {"name": "Hau"},"2": {"name": "Thy"},"3": {"name": "Toan"}]';
-
     this.setStore("devlist", originStore);
+    this.init();
   }
 
 /*
@@ -26,12 +26,18 @@ var CurdList = (function () {
   "3": {"name": "Toan"}
 }
 */
+  CurdList.prototype.init = function () {
+    this.addItem();
+    this.removeAllItems();
+    this.addRemovingEventForItems();
+  }
+
 
   CurdList.prototype.getStore = function (storeName) {
     var store = localStorage.getItem(storeName);
     try
     {
-      store = JSON.parse(store); 
+      store = JSON.parse(store);
     }
     catch(e)
     {
@@ -44,7 +50,24 @@ var CurdList = (function () {
     localStorage.setItem(storeName, data);
   }
 
-
+  CurdList.prototype.updateShowList = function () {
+    var store = this.getStore('devlist');
+    if ( store !== null ) {
+      var showListEle = this.d.querySelector('[data-showlist]');
+      showListEle.innerHTML = '';
+      for( var k in store ) {
+        if ( store.hasOwnProperty(k) ) {
+          var liEle = this.d.createElement('li');
+          liEle.innerHTML = store[k].name;
+          liEle.setAttribute('data-id', k);
+          var btnEle = this.d.createElement('button');
+          btnEle.innerHTML = "delete";
+          liEle.appendChild(btnEle);
+          showListEle.appendChild(liEle);
+        }
+      }
+    }
+  }
 
   CurdList.prototype.addItem = function () {
     var that = this;
@@ -64,17 +87,49 @@ var CurdList = (function () {
       }
       localStorage.setItem("devlist", JSON.stringify(newStore));
       document.querySelector('[data-name]').value = "";
+      // 
+      that.updateShowList();
     });
+
+  }
+
+  CurdList.prototype.removeAllItems = function () {
+    var that = this;
+    var removeAllButton = document.querySelector('[data-btn-clearAll]');
+    removeAllButton.addEventListener("click", function () {
+      localStorage.clear();
+    });
+  }
+  
+  CurdList.prototype.addRemovingEventForItems = function () {
+    var that = this;
+    var removeButton = document.querySelectorAll('[data-remove]');
+    this.d.addEventListener("mousedown", function (event) {
+      let real_target = event.target;
+      let container_target = that.getClosestTargetByAttrName(real_target, 'data-id');
+      
+      if ( container_target !== null ) {
+        console.log(container_target);
+        
+        var id = container_target.getAttribute('data-id');
+        var store = that.getStore("devlist");
+        delete store[id];
+        that.setStore('devlist', JSON.stringify(store));
+        that.updateShowList();
+      }
+    });
+
+  }
+
+  CurdList.prototype.getClosestTargetByAttrName = function (el, attrName) {
+    while ( (el = el.parentElement) && !el.hasAttribute(attrName) );
+    return el;
   }
 
   return CurdList;
 }());
 
- 
-
 // instance
 window.addEventListener('DOMContentLoaded', function () {
-  
-  var add_name = new CurdList("hello word");
-  add_name.addItem();
+  var mangageList = new CurdList();
 });
